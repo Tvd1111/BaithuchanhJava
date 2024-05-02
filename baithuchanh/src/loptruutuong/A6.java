51c4f056f5d9adcbad9d5b53415a24ff1fcbdee3
Tvd1111 <tongvandoai1111@gmail.com>
2024-05-02 18:38:52 +0700

    baitap 26/4

---
+++ baithuchanh/src/loptruutuong/A6.java
@@ -0,0 +1,9 @@
+public class A6 implements printable{
+    public void print(){
+        System.out.println("Hello");
+    }
+    public static void main(String[] args) {
+        A6 obj = new A6();
+        obj.print();
+    }
+}
